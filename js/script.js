f310c85d3f66cea028dcad7c63eeb7cc6f783e0a
francescoP02 <file.partipilo@gmail.com>
2022-05-13 15:42:00 +0200

    Added generation of array

--- js/script.js
+++ js/script.js
@@ -2,11 +2,30 @@ const app = new Vue(
     {
         el: `#root`,
         data: {
+            emailArray : [],
 
         },
 
         methods: {
+            getEmail: function() {
+                let fetchedEmail = ''
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    generatedEmail = response.data.response;
+                    console.log(generatedEmail);
+                    this.emailArray.push(generatedEmail);
+                }); 
+            },
 
+            buildEmailArray: function(numberofEmail) {
+                this.emailArray = [];
+                for (let i = 0; i < numberofEmail; i++) {
+                    this.getEmail();
+                }
+            },
+        },
+        mounted() {
+            this.buildEmailArray(this.howManyToGenerate);
         },
     }
 ) 
